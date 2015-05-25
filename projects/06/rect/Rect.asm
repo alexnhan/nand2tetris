b90b2b2c2140c458c0789118f68d2871ade1d4d6
Alex Nhan <nhanalex320@gmail.com>
2015-05-24 21:40:13 -0400

    Finished editing and testing assembler. Assembler_Symbols handles all assembling tests

--- projects/06/rect/Rect.asm
+++ projects/06/rect/Rect.asm
@@ -6,30 +6,30 @@
 // Draws a rectangle at the top-left corner of the screen.
 // The rectangle is 16 pixels wide and R0 pixels high.
 
-   @0
-   D=M
-   @INFINITE_LOOP
-   D;JLE 
-   @counter
-   M=D
-   @SCREEN
-   D=A
-   @address
-   M=D
+@0
+D=M
+@INFINITE_LOOP
+D;JLE 
+@counter
+M=D
+@SCREEN
+D=A
+@address
+M=D
 (LOOP)
-   @address
-   A=M
-   M=-1
-   @address
-   D=M
-   @32
-   D=D+A
-   @address
-   M=D
-   @counter
-   MD=M-1
-   @LOOP
-   D;JGT
+@address
+A=M
+M=-1
+@address
+D=M
+@32
+D=D+A
+@address
+M=D
+@counter
+MD=M-1
+@LOOP
+D;JGT
 (INFINITE_LOOP)
-   @INFINITE_LOOP
-   0;JMP
+@INFINITE_LOOP
+0;JMP
