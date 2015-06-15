670cc357aea0c8f7696f83bea37ecb707c4d1f05
Alex <nhanalex320@gmail.com>
2015-06-15 18:13:41 -0400

    Update Parser.h

--- projects/07/Parser.h
+++ projects/07/Parser.h
@@ -3,30 +3,31 @@
 
 #include <fstream>
 #include <string>
+using namespace std;
+
+// VM commands
+enum VMcommand { C_ARITHMETIC, C_PUSH, C_POP, C_LABEL, C_GOTO, C_IF, C_FUNCTION, C_RETURN, C_CALL };
 
 class Parser
 {
-	public:
-		// VM commands
-		enum VMcommand { C_ARITHMETIC, C_PUSH, C_POP, C_LABEL, C_GOTO, C_IF, C_FUNCTION, C_RETURN, C_CALL };
+public:
+	// Constructor
+	Parser(string filename);
 
-		// Constructor
-		Parser(string filename);
+	// Destructor
+	~Parser();
 
-		// Destructor
-		~Parser();
+	// Methods
+	bool hasMoreCommands(void); // are there more commands in the input?
+	void advance(void); // reads next command from input, should only be called if hasMoreCommands() is true
+	VMcommand commandType(void); // returns the type of the current VM command
+	string arg1(void); // returns the first argument of the current command
+	int arg2(void); // returns the second argument of the current command
 
-		// Methods
-		bool hasMoreCommands() const; // are there more commands in the input?
-		void advance(); // reads next command from input, should only be called if hasMoreCommands() is true
-		VMcommand commandType(); // returns the type of the current VM command
-		string arg1(); // returns the first argument of the current command
-		int arg2(); // returns the second argument of the current command
-
-	private: // Attributes
-		string filename_;
-		string currentCommand_;
-		ifstream inf;
+private: // Attributes
+	string filename_;
+	string currentCommand_;
+	ifstream inf;
 };
 
 #endif
